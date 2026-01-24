0366eeca0034a8c12a27af60e4c7e699968ad551
j0nascz <Jonascz2004@gmail.com>
2026-01-24 11:42:06 +0100

    lösung

--- Übunsblatt4_4.3b.R
+++ Übunsblatt4_4.3b.R
@@ -17,5 +17,5 @@ for(k in c(5, 10, 20, 50, 100, 200)){
   approx_k <- svd_pic$u[, 1:k] %*% diag(svd_pic$d[1:k]) %*% t(svd_pic$v[, 1:k]) 
   image(t(approx_k), col = grey(seq(0, 1, length = 256))) 
   title(main = paste0("k = ", k, ", gespeicherte Zahlen: ", 2*n_pic*k + k,
-                      ", ca ", round((2*n_pic*k + k)/n_picˆ2*100), "% vom Original"),
+                      ", ca ", round((2*n_pic*k + k)/n_pic^2*100), "% vom Original"),
                       cex.main = 0.5) }
